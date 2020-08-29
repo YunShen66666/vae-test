5532b91ff5c67ce9836794c66baf28f45d716292
Yunshen <578431642@qq.com>
2020-08-29 17:25:59 +0800

    卷积版ae

--- encoder.py
+++ encoder.py
@@ -3,24 +3,33 @@ import torch.nn as nn
 class auto_Encoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(28*28,100),
-            nn.ReLU(),
-            nn.Linear(100,10),
-            nn.ReLU(),
-            nn.Linear(10,2),
-            nn.ReLU()
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(2, 10),
-            nn.ReLU(),
-            nn.Linear(10, 100),
-            nn.ReLU(),
-            nn.Linear(100, 28*28),
-            nn.Sigmoid()
-        )
-    def forward(self,x):
+        self.encoder_block1 = self.set_block(1,5)
+        self.encoder_block2 = self.set_block(5,10)
+        self.encoder_block3 = self.set_block(10,20,relu=False)
+        self.decoder_block3 = self.set_block(20,10)
+        self.decoder_block2 = self.set_block(10,5)
+        self.decoder_block1 = self.set_block(5,1,relu=False)
 
-        latent = self.encoder(x)
-        output = self.decoder(latent)
-        return latent,output
+        self.pool = nn.MaxPool2d(2,return_indices=True)
+        self.unpool = nn.MaxUnpool2d(2)
+
+    def set_block(self,input,output,relu=True):
+        self.block = nn.Sequential(nn.Conv2d(input,output,3,padding=1),
+                                   nn.BatchNorm2d(output),
+                                   nn.ReLU() if relu==True else nn.Tanh()
+                                   )
+        return self.block
+
+    def forward(self,x):
+        x = self.encoder_block1(x) #28*28
+        x,index1 = self.pool(x)   #14*14
+        x = self.encoder_block2(x) #14*14
+        x,index2 = self.pool(x)  #7*7
+        latent = self.encoder_block3(x) #7*7
+
+        x = self.decoder_block3(latent) #7*7
+        x = self.unpool(x,index2) #14*14
+        x = self.decoder_block2(x) #14*14
+        x = self.unpool(x,index1) #28*28
+        x = self.decoder_block1(x)
+        return latent,x
--- train_anto_encoder.py
+++ train_anto_encoder.py
@@ -6,29 +6,30 @@ import time
 
 BATCH_SIZE = 1000
 epoch = 20
-learning_rate = 0.0001
+learning_rate = 0.001
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 transforms = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5],[0.5])])
 mnist = datasets.MNIST(root='./',train=True,transform=transforms,download=True)
-#print(mnist)
+#print(mnist[0][0])
 
 dataloader = torch.utils.data.DataLoader(mnist,batch_size=BATCH_SIZE,shuffle=True)
 
 auto_E = auto_Encoder().to(DEVICE)
 optimizer = torch.optim.SGD(auto_E.parameters(),lr=learning_rate)
-citizerion = nn.BCELoss()
+citizerion = nn.MSELoss()
 
 
 for i in range(epoch):
     a = time.time()
+    auto_E.train()
     for index,(img,_) in enumerate(dataloader):
-        x = img.view(-1,28*28).to(DEVICE)
+        x = img.to(DEVICE)
         latent,output = auto_E(x)
         loss = citizerion(output,x)
         loss.backward()
         optimizer.step()
-
+        b = time.time()
         if index%10==9:
             b = time.time()
             print("epoch={},batch={},loss={:.4f},speed={:.6f}/10个batch".format(i,index,loss,index/(b-a)))
